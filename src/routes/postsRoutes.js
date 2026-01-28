5c38143b228bb3a38829e97456f0a651d36cb32a
Anderson <ilesanderson01@gmail.com>
2026-01-27 21:25:02 -0500

    Adición CRUD basico para posteos

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -5,10 +5,10 @@ const { authMiddleware } = require('../middleware/auth');
 
 const router=express.Router();
 
-
+router.use(authMiddleware)
 router.post('/',postControllers.createPost);
 
-router.get('/',authMiddleware,postControllers.getAllPost)
+router.get('/',postControllers.getAllPost)
 
 
 module.exports=router

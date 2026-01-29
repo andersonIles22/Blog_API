5069ef2c570cd8f64c6680fadda3350bc1eb8593
Anderson <ilesanderson01@gmail.com>
2026-01-29 16:43:27 -0500

    Adición de mensajes y valores de validación

--- src/constants/messagesValidation.js
+++ src/constants/messagesValidation.js
@@ -9,7 +9,9 @@ const MESSAGES_VALIDATION=({
     NEW_PASSWORD_TOO_SHORT:"New Password must be at least 6 characters",
     NEW_PASS_NO_EQUAL_CONFIRM_PASS:"The Confirm Password does not match the New Password",
     NEW_PASSWORD_IS_EQUAL_TO_CURRENT_PASSWORD:"New password must be different from current password",
-    MUST_BE_A_INTEGER:"Id should be a Integer Positive"
+    MUST_BE_A_INTEGER:"Id should be a Integer Positive",
+    COMMENT_IS_EMPTY:"Comment is required",
+    COMMENT_TOO_SHORT:"Comment must be at least 4 characters"
 });
 module.exports={
     MESSAGES_VALIDATION

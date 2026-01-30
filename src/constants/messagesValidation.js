8e22a1f1a6956af3c22f4f6a10e954ff36c3674e
Anderson <ilesanderson01@gmail.com>
2026-01-29 19:56:13 -0500

    Adicción de valores y mensajes de validación

--- src/constants/messagesValidation.js
+++ src/constants/messagesValidation.js
@@ -10,8 +10,12 @@ const MESSAGES_VALIDATION=({
     NEW_PASS_NO_EQUAL_CONFIRM_PASS:"The Confirm Password does not match the New Password",
     NEW_PASSWORD_IS_EQUAL_TO_CURRENT_PASSWORD:"New password must be different from current password",
     MUST_BE_A_INTEGER:"Id should be a Integer Positive",
+    TITLE_POSTS_MIN_AND_MAX_CHARACTERS:"The Title should be between 5 to 200 characters",
+    CONTENT_POSTS_MIN_CHARACTERS:"The content should be a least 20 characters",
     COMMENT_IS_EMPTY:"Comment is required",
-    COMMENT_TOO_SHORT:"Comment must be at least 4 characters"
+    TITLE_POST_IS_EMPTY:"Title is required",
+    CONTENT_POST_IS_EMPTY:"Content is required",
+    COMMENT_LIMIT_CHARACTERS:"Comment should be between 3 to 500 characters"
 });
 module.exports={
     MESSAGES_VALIDATION
--- src/constants/statusMessages.js
+++ src/constants/statusMessages.js
@@ -1,7 +1,6 @@
 
 const MESSAGES_OPERATION= Object.freeze({
     SUCCESFUL_OPERATION:"Successful Operation",
-    TITTLE_REQUIRED:"Title is required",
     SERVER_ERROR:"Internal Server Error",
     EMAIL_ALREADY_EXIST:"Email already registered",
     LOGIN_SUCCESSFULLY:"Successful Login",
@@ -14,8 +13,10 @@ const MESSAGES_OPERATION= Object.freeze({
     TOKEN_EXPIRED:'Token has expired',
     URL_NO_FOUND:(url)=>`Path: ${url}, no Found`,
     NOT_AUTHENTICATED:"No Authenticated",
-    DENIED_ACCESS:"Access Denied. Insufficient Permissions"
+    DENIED_ACCESS:"Access Denied. Insufficient Permissions",
+    NOT_IS_AUTHOR:"You do not have permissions to edit this post"
 });
+
 module.exports={
     MESSAGES_OPERATION
 }

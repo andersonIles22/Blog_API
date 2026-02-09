e00daeb37232792bb9528ee6c5823ae34f3dadbc
Anderson <ilesanderson01@gmail.com>
2026-02-09 16:43:11 -0500

    Agregar paginación a los endpoints posts y comments

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -61,7 +61,7 @@ const getPostcomments=async (req,res,next) => {
             `,
             [post_id,limit,offset]
         );
-        const comments=queryGetCommentsOnPost.rows[0];
+        const comments=queryGetCommentsOnPost.rows;
         const numberOfPages=Math.ceil(comments.length/limit);
         if(page>numberOfPages) return error(HTTP_STATUS.BAD_REQUEST,MESSAGES_OPERATION.NUMBER_PAGE_NOT_FOUND,next);
 

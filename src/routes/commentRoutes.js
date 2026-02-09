96515f58f12abe5e20f33a9b8261a76d047a616f
Anderson <ilesanderson01@gmail.com>
2026-02-09 12:36:34 -0500

    Agregar paginación a los endpoints posts y comments

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,12 +1,12 @@
 const express=require('express');
-const { validateIdPost,validateCommentPos,validateQueryGetPosts } = require('../middleware/validationInputs');
+const { validateIdPost,validateCommentPost,validateQueryGetPosts } = require('../middleware/validationInputs');
 const { authMiddleware } = require('../middleware/auth');
 const commentControllers=require('../controllers/commentController');
 
 
 const router=express.Router({mergeParams:true});
 
-router.post('/',authMiddleware,validateIdPost,validateQueryGetPosts,commentControllers.postsComment);
+router.post('/',authMiddleware,validateIdPost,validateCommentPost,commentControllers.postsComment);
 
 router.get('/',validateIdPost,validateQueryGetPosts,commentControllers.getPostcomments)
 

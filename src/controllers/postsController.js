766927d278f1c69f68c5621dc435f8ea9dbb7385
Anderson <ilesanderson01@gmail.com>
2026-02-11 19:04:02 -0500

    Agregar verificación de propietario del posts para actualizar o eliminar el posts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -128,16 +128,18 @@ const updatePost=async (req,res,next) => {
         const countPosts=queryGetPosts.rows.length;
         if(!countPosts>0) return error(HTTP_STATUS.NOT_FOUND,MESSAGES_OPERATION.POST_NOT_FOUND,next);
 
+        // Estructuramos la consulta a la base de datos dependiendo de si es admin o el propietario
+        const queryBase=`UPDATE posts
+            SET title=COALESCE($1,title), content=COALESCE($2,content)`;
+
+        const whereClause=(role==='admin')? `WHERE id=$3 RETURNING *`:`WHERE id=$3 AND author_id=$4 RETURNING *`
+        
+        const queryResult=`${queryBase} ${whereClause}`;
+
         const queryUpdatePost= await db.query(
-            `UPDATE posts
-            SET title=COALESCE($1,title), content=COALESCE($2,content)
-            WHERE id=$3 AND author_id=$4
-            RETURNING *`,
+            queryResult,
             [title,content,post_id,id]
         );
-
-
-        if(queryUpdatePost.rowCount===0) return error(HTTP_STATUS.FORBIDDEN,MESSAGES_OPERATION.NOT_IS_AUTHOR,next);
 
         res.status(HTTP_STATUS.OK).json(
         {

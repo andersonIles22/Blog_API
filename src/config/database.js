acf68bb13b2da66fc38cb19719b95e99d16c7362
Anderson <ilesanderson01@gmail.com>
2026-02-26 20:11:28 -0500

    feat(config): implement conditional db connection for test and production environments

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,11 @@
 const {Pool}= require('pg');
 require('dotenv').config();
+
+const {DATABASE_URL_AUTH_DB,DATABASE_URL_AUTH_DB_TEST}=process.env;
+const  connectionString=process.env.NODE_ENV==='test'? process.env.DATABASE_URL_AUTH_DB_TEST:process.env.DATABASE_URL_AUTH_DB;
+
 const pool=new Pool({
-    connectionString:process.env.DATABASE_URL_AUTH_DB,
+    connectionString:connectionString,
     ssl:process.env.NODE_ENV==='production'?{rejectUnauthorized:false}:false
 });
 module.exports=pool;

5bd2ff946a571c6163a5a6ff404d29e7970030d1
andersonIles22 <ilesanderson01@gmail.com>
2026-03-25 15:39:19 -0500

    feat(validations): add message about email validation

--- src/constants/messagesValidation.js
+++ src/constants/messagesValidation.js
@@ -1,6 +1,7 @@
 const MESSAGES_VALIDATION=({
     EMAIIL_TOO_LONG:"Email must be 255 chareacters or less",
     EMAIL_INVALID:"Valid email required",
+    EMAIL_REQUIRED:"Email required",
     PASSWORD_TOO_SHORT:"Password must be at least 6 characters",
     PASSWORD_EMPTY:"Password required",
     NAME_REQUIRED:"Name is required",

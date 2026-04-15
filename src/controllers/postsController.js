7f45474954a29dda436c4f6289488dfa04c7f950
andersonIles22 <ilesanderson01@gmail.com>
2026-04-15 12:49:01 -0500

    refactor(posts): extract getPostById, getAllPost, updatePost logic to PostsService

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -3,7 +3,7 @@ const db=require('../config/database');
 const {HTTP_STATUS}=require('../constants/httpStatusCode');
 const {MESSAGES_OPERATION}=require('../constants/statusMessages');
 const { matchedData } = require('express-validator');
-const {createPost}=require('../services/postsService')
+const postsService=require('../services/postsService')
 
 
 //#region INSERT
@@ -18,7 +18,7 @@ const createPosts=async (req,res,next) => {
     }
 
     try {
-        const result= await createPost(postData);
+        const result= await postsService.createPost(postData);
         res.status(HTTP_STATUS.CREATED).json({
             success:true,
             message:"Post published successfully",
@@ -31,17 +31,13 @@ const createPosts=async (req,res,next) => {
 
 //#region  GET ById
 const getPostById=async (req,res,next) => {
-    try {
-        const id=parseInt(req.params.post_id);
-        const queryGetById= await db.query(
-            `SELECT * FROM posts WHERE id=$1`,
-            [id]
-        )
-        const post=queryGetById.rows[0];
+    const id=parseInt(req.params.post_id);
 
-        if(!post) return error(HTTP_STATUS.NOT_FOUND,MESSAGES_OPERATION.POST_NOT_FOUND,next);
+    try {
+        const result=await postsService.findById(id);
+    
         res.status(HTTP_STATUS.OK).json({
-            post:post
+            post:result
         })
     } catch (error) {
         next(error);
@@ -50,116 +46,21 @@ const getPostById=async (req,res,next) => {
 //#region GET ALL
 
 const getAllPost=async(req,res,next)=>{
+    const validData=matchedData(req)
+    const params={
+        page:validData.page,
+        limit:validData.limit,
+        author_id:validData.author_id,
+        published:validData.published,
+        category:validData.category
+    }
     try {
-        const conditionArr=[];
-        const valuesArr=[];
-        const baseQuery=
-        `SELECT 
-            p.id,
-            p.title,
-            p.content,
-            json_build_object(
-                'id',u.id,
-                'name',u.name
-            ) as author,
-            COALESCE(
-                JSON_AGG(
-                    json_build_object(
-                    'id',p_c.category_id,
-                    'name',cat.name
-                    )
-                ) FILTER (where p_c.category_id IS not NULL)
-            ,'[]')  as category_ids
-        FROM users u
-        JOIN posts p ON u.id=p.author_id
-        LEFT JOIN post_categories p_c ON p.id=p_c.post_id
-        LEFT JOIN categories cat ON p_c.category_id=cat.id
-        `;
-
-        const {page,limit,author_id,published,category}=matchedData(req);
-        // Validamos que los parametros existan y asi establecer las condicionales en caso de indicar en la ruta
-        if(author_id){
-            valuesArr.push(author_id)
-            let number=valuesArr.length
-            conditionArr.push(`p.author_id=$${number}`)
-        }
-        if(published){
-            valuesArr.push(published)
-            let number=valuesArr.length
-            conditionArr.push(`p.published=$${number}`)
-        }
-        if(category){
-            valuesArr.push(category)
-            let number=valuesArr.length
-            conditionArr.push(`LOWER(cat.name)=$${number}`)
-        }
-
-        let whereConditions=conditionArr.join(` AND `)
-        if(conditionArr.length>0){
-            whereConditions=` WHERE ${whereConditions}`
-        }
-        // Se establece una consulta a la db para obtener el numero total 
-        // de publicaciones en base a las condiciones establecidas antes de agregar los parametros LIMIT y OFFSET
-        let QueryAllPost= `
-            SELECT COUNT(DISTINCT p.id)
-            FROM posts p
-            LEFT JOIN post_categories p_c ON p.id=p_c.post_id
-            LEFT JOIN categories cat ON p_c.category_id=cat.id 
-            ${whereConditions}`
-
-        const queryGetAllPost= await db.query(
-            QueryAllPost, valuesArr
-        )
-        const numberOfPosts=queryGetAllPost.rows[0].count;
-        const numberOfPages=Math.ceil(numberOfPosts/limit);
-
-        if(numberOfPosts>0 && page>numberOfPages){
-        return res.status(HTTP_STATUS.OK).json({
-            success:true,
-            message:"Not posts found for this page",
-            pagination:{
-                totalPosts:numberOfPosts,
-                totalPages:numberOfPages,
-                currentPage:page,
-                pageSize:limit
-            },
-            data:[]
-        });
-        }    
-
-        
-        // LIMIT y OFFSET al final para evitar problemas con el conteo de posts aplicando o no los filtros
-        let limitePage="";
-        const offset=(page-1)*limit;
-        if(limit && page){
-            valuesArr.push(limit,offset)
-            let number=valuesArr.length
-            limitePage=`LIMIT $${number-1} OFFSET $${number}`
-        }
-
-        let resultQuery=`
-            ${baseQuery} 
-            ${whereConditions} 
-            GROUP BY p.id, u.id
-            ORDER BY p.created_at DESC 
-            ${limitePage}`;
-
-        // Consulta con todos los parametros establecidos en la query de la ruta
-        const queryGetPostLimited=await db.query(
-            resultQuery,valuesArr
-        );
-
-
+        const result= await postsService.findAllPosts(params)
         res.status(HTTP_STATUS.OK).json({
-            success:true,
-            message:"Get data successfully",
-            pagination:{
-                totalPosts:numberOfPosts,
-                totalPages:numberOfPages,
-                currentPage:page,
-                pageSize:limit
-            },
-            data:queryGetPostLimited.rows
+            success:result.success,
+            message:result.message,
+            pagination:result.pagination,
+            data:result.data
         });
     } catch (error) {
         next(error);
@@ -168,29 +69,17 @@ const getAllPost=async(req,res,next)=>{
 //#region UPDATE
 const updatePost=async (req,res,next) => {
     try {
-        const {post_id}=req.params;
-        const {title,content}=req.body;
-
-        //Verificamos si el post existe 
-        const queryGetPosts= await db.query(
-            `SELECT * FROM posts WHERE id=$1`,[post_id]
-        );
-        const countPosts=queryGetPosts.rows.length;
-        if(!countPosts>0) return error(HTTP_STATUS.NOT_FOUND,MESSAGES_OPERATION.POST_NOT_FOUND,next);
-
-        const queryUpdatePost= await db.query(
-            `UPDATE posts
-            SET title=COALESCE($1,title), content=COALESCE($2,content)
-            WHERE id=$3`,
-            [title,content,post_id]
-        );
-
-        res.status(HTTP_STATUS.OK).json(
-        {
-            success:true,
-            message:MESSAGES_OPERATION.SUCCESFUL_OPERATION
+        const dataPost={
+            post_id:req.params.post_id,
+            title:req.body.title,
+            content:req.body.content
         }
-        );
+        const result=await postsService.update(dataPost)
+
+        res.status(HTTP_STATUS.OK).json({
+            success:true,
+            message:MESSAGES_OPERATION.SUCCESFUL_OPERATION,
+        });
 
     } catch (error) {
         next(error)
--- src/services/postsService.js
+++ src/services/postsService.js
@@ -1,4 +1,8 @@
 const db=require('../config/database');
+const { HTTP_STATUS } = require('../constants/httpStatusCode');
+const { MESSAGES_OPERATION } = require('../constants/statusMessages');
+const AppError=require('../utils/appError')
+
 
 const createPost=async (postData) => {
     const client=await db.connect();
@@ -52,6 +56,159 @@ const createPost=async (postData) => {
 
 };
 
+const findById=async (resource_id) => {
+    const getPostQuery=await db.query(
+        `SELECT * FROM posts WHERE id=$1`,
+        [resource_id]
+    );
+    if (!getPostQuery.rows[0]) throw new AppError(MESSAGES_OPERATION.POST_NOT_FOUND,HTTP_STATUS.NOT_FOUND)
+    return {...getPostQuery.rows[0]};
+}
+
+const findAllPosts=async (params) => {
+    const conditionArr=[];
+    const valuesArr=[];
+    const baseQuery=
+    `SELECT 
+        p.id,
+        p.title,
+        p.content,
+        json_build_object(
+            'id',u.id,
+            'name',u.name
+        ) as author,
+        COALESCE(
+            JSON_AGG(
+                json_build_object(
+                'id',p_c.category_id,
+                'name',cat.name
+                )
+            ) FILTER (where p_c.category_id IS not NULL)
+        ,'[]')  as category_ids
+    FROM users u
+    JOIN posts p ON u.id=p.author_id
+    LEFT JOIN post_categories p_c ON p.id=p_c.post_id
+    LEFT JOIN categories cat ON p_c.category_id=cat.id
+    `;
+
+    const {page,limit,author_id,published,category}=params;
+    // Validamos que los parametros existan y asi establecer las condicionales en caso de indicar en la ruta
+    if(author_id){
+        valuesArr.push(author_id)
+        let number=valuesArr.length
+        conditionArr.push(`p.author_id=$${number}`)
+    }
+    if(published){
+        valuesArr.push(published)
+        let number=valuesArr.length
+        conditionArr.push(`p.published=$${number}`)
+    }
+    if(category){
+        valuesArr.push(category)
+        let number=valuesArr.length
+        conditionArr.push(`LOWER(cat.name)=$${number}`)
+    }
+
+    let whereConditions=conditionArr.join(` AND `)
+    if(conditionArr.length>0){
+        whereConditions=` WHERE ${whereConditions}`
+    }
+    // Se establece una consulta a la db para obtener el numero total 
+    // de publicaciones en base a las condiciones establecidas antes de agregar los parametros LIMIT y OFFSET
+    let QueryAllPost= `
+        SELECT COUNT(DISTINCT p.id)
+        FROM posts p
+        LEFT JOIN post_categories p_c ON p.id=p_c.post_id
+        LEFT JOIN categories cat ON p_c.category_id=cat.id 
+        ${whereConditions}`
+
+    const queryGetAllPost= await db.query(
+        QueryAllPost, valuesArr
+    )
+    const numberOfPosts=queryGetAllPost.rows[0].count;
+    const numberOfPages=Math.ceil(numberOfPosts/limit);
+
+    let message;
+    let data;
+
+    if(numberOfPosts>0 && page>numberOfPages){
+        message="Not posts found for this page"
+        data=[]
+    };    
+
+    // LIMIT y OFFSET al final para evitar problemas con el conteo de posts aplicando o no los filtros
+    let limitePage="";
+    const offset=(page-1)*limit;
+    if(limit && page){
+        valuesArr.push(limit,offset)
+        let number=valuesArr.length
+        limitePage=`LIMIT $${number-1} OFFSET $${number}`
+    }
+
+    let resultQuery=`
+        ${baseQuery} 
+        ${whereConditions} 
+        GROUP BY p.id, u.id
+        ORDER BY p.created_at DESC 
+        ${limitePage}`;
+
+    // Consulta con todos los parametros establecidos en la query de la ruta
+    const queryGetPostLimited=await db.query(
+        resultQuery,valuesArr
+    );
+
+    return {
+        success:true,
+        message:message||'Get Data Successfully',
+        pagination:{
+            totalPosts:numberOfPosts,
+            totalPages:numberOfPages,
+            currentPage:page,
+            pageSize:limit
+        },
+        data:data||queryGetPostLimited.rows
+    }
+}
+
+const update= async(post)=>{
+    const {post_id,title,content}=post;
+    try {
+        const queryUpdatePost= await db.query(
+            `UPDATE posts
+            SET title=COALESCE($1,title), content=COALESCE($2,content)
+            WHERE id=$3
+            RETURNING *`,
+            [title,content,post_id]
+        );
+        if (!queryUpdatePost.rows[0]) throw new AppError(MESSAGES_OPERATION.POST_NOT_FOUND,HTTP_STATUS.NOT_FOUND);   
+    } catch (error) {
+        throw error
+    }
+}
+
+
+const checkOwnerShip=async (data) => {
+    const {field,resourceType,resourceId}=data;
+    const getOwnerIdQuery=`
+    SELECT ${field} FROM ${resourceType}
+    WHERE id=$1
+    `;
+
+    const getOwnerIdOfResource= await db.query(
+        getOwnerIdQuery,
+        [resourceId]
+    );
+
+    return getOwnerIdOfResource.rows[0][field];
+}
+/*
+Estabamos implementando el service Update pero surgio el problema de
+separar acciones con el middleware isOwnerOrAdmin. 
+*/
 module.exports={
-    createPost
+    createPost,
+    findById,
+    findAllPosts,
+    update,
+    checkOwnerShip
 };

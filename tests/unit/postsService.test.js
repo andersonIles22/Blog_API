b34a73cea40d1ba50a1712c6e61fd1f25206d9d1
andersonIles22 <ilesanderson01@gmail.com>
2026-05-15 22:56:06 -0500

    test(postService): add unit test (success and fail) to the endpoint api/posts/:post_id

--- tests/unit/postsService.test.js
+++ tests/unit/postsService.test.js
@@ -1,6 +1,8 @@
 const postsService=require('../../src/services/postsService');
 const postRepository=require('../../src/repositories/postsRepository');
 const userRepository=require('../../src/repositories/userRepository');
+const { error } = require('../../src/middleware/errorHandler');
+const { param } = require('express-validator');
 
 jest.mock('../../src/repositories/userRepository')
 jest.mock('../../src/repositories/postsRepository');
@@ -52,16 +54,28 @@ describe('GET api/posts - Get all Posts',()=>{
 })
 
 describe('GET api/posts/id: - Get post by id',()=>{
-    const mockFailMessage='The Post does not exist'
-    beforeEach(()=>{
-        jest.clearAllMocks();
-        postRepository.getById.mockResolvedValue(null)
+    const mockPosts={
+        id:1,
+        title:'La ballena azul',
+        content:'Trata sobre una ballena azul, xd',
+        author_id:2,
+        published:true
+    }
+    const resource_id=1
+    const resource_id_rejected=2
+
+    beforeEach(() => { 
+        jest.clearAllMocks();   
+        postRepository.getById.mockResolvedValue(mockPosts)
+    });
+
+    it('Get the post by id succesfully',async () => {
+        const result=await postsService.findById(resource_id)
+        expect(result).toEqual(mockPosts)
     })
-    
-    const resource_id=44
-    it('it should fail if the post id is invalid ', async () => {
-        await expect(postsService.findById(resource_id)) 
-            .rejects
-            .toThrow(mockFailMessage)
+
+    it('Should fail if the post does not exist', async () => {
+        postRepository.getById.mockResolvedValue(null)
+        await expect(postsService.findById(resource_id_rejected)).rejects.toThrow('The Post does not exist')
     })
 })

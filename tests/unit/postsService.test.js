a02d4533ec3786b7293111000ac77509dc5fe5e6
andersonIles22 <ilesanderson01@gmail.com>
2026-07-22 19:40:29 -0500

    test(postService): implement unit test for findAllPosts Service

--- tests/unit/postsService.test.js
+++ tests/unit/postsService.test.js
@@ -9,25 +9,47 @@ const userRepository=require('../../src/repositories/userRepository');
 
 describe('GET api/posts',()=>{
 
-    const mockPost={
+    const mockPosts=[
+        {
         author_id:1,
         content:"El titulo es la frase que pensaba que lo dijo Justin Beiber",
         id:1,
         published:true,
         title:"Nunca digas nunca"
-    }
+        },
+        {
+        author_id:1,
+        content:"Dime que nunca has amado sin, decirmo que nunca has amado",
+        id:2,
+        published:true,
+        title:"Una confesión"
+        }
+]
 
     beforeEach(()=>{
         jest.clearAllMocks();
-        postRepository.getById.mockResolvedValue(mockPost)
+        userRepository.checkAuthor.mockResolvedValue(true)
+        postRepository.countPosts.mockResolvedValue(2)
+        postRepository.getById.mockResolvedValue(mockPosts[0])
+        postRepository.getAll.mockResolvedValue(mockPosts)
     })
     
     
     const postId=1
-    
+    const params={
+        page:1,
+        limit:3,
+        author_id:1,
+        published:true
+    }
     it('Get a post by Id',async () => {  
-        const result = await postsService.findById('1');
-        expect(result).toEqual(mockPost)
+        const result = await postsService.findById(postId);
+        expect(result).toEqual(mockPosts[0])
         expect(postRepository.getById).toHaveBeenCalledTimes(1)
+    })
+    it('Get All posts sucessfully',async () => {
+        const result = await postsService.findAllPosts(params)
+        expect(result.data).toEqual(mockPosts)
+        expect(postRepository.getAll).toHaveBeenCalledTimes(1)
     })
 })

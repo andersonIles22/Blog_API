72140b1513af575422b33710178282ecd8bc286a
andersonIles22 <ilesanderson01@gmail.com>
2026-07-31 19:14:33 -0500

    feat(test): add integration test for PATCH /api/posts/:id endpoint

---
+++ tests/integration/post.patch.test.js
@@ -0,0 +1,65 @@
+const supertest=require('supertest');
+const app=require('../../src/app');
+const {cleanDataBase, seedUsersWithRolePostsTestData}=require('../helpers/database.setup');
+const {generateToken}=require('../helpers/auth.test.helper')
+
+const api=supertest(app);
+
+    let postsIds;  
+    let ramdonUser;
+    let mainUser;
+    let adminUser;
+    let tokenRamdonU;
+    let tokenMainU;
+    let tokenAdmin;
+describe('PATCH /api/posts/:id',()=>{
+
+    let postsIds;  
+    let ramdonUser;
+    let mainUser;
+    let adminUser;
+    let tokenRamdonU;
+    let tokenMainU;
+    let tokenAdmin;
+
+    const baseUrl='/api/posts/'
+
+    beforeAll(async () => {
+        await cleanDataBase();
+        const {users,postsId}=await seedUsersWithRolePostsTestData();
+        ramdonUser=users[0];
+        mainUser=users[1];
+        adminUser=users[2];
+        postsIds=postsId;
+        tokenAdmin= generateToken(adminUser);
+        tokenRamdonU=generateToken(ramdonUser);
+        tokenMainU=generateToken(mainUser);
+    })
+    afterAll(async () => {
+        await cleanDataBase()
+    })
+
+    describe('Tests Pass Successfully',()=>{
+        it('status 200 if update a post by the owner',async () => {
+            const validPostId=postsIds[2];
+            const response= await api
+                .patch(`${baseUrl}${validPostId}`)
+                .set('Authorization',`Bearer ${tokenMainU}`)
+                .send({
+                    title:"Pongase Serio socio"
+                })
+            expect(response.status).toBe(200)
+        })
+
+        it('status 200 if update a post by the admin',async () => {
+            const validPostId=postsIds[2];
+            const response= await api
+                .patch(`${baseUrl}${validPostId}`)
+                .set('Authorization',`Bearer ${tokenAdmin}`)
+                .send({
+                    title:"Pongase Serio socio"
+                })
+            expect(response.status).toBe(200)
+        })
+    })
+})

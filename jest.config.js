7a6b2c40fdae2e6b017d9bb5c81c620595dd165c
andersonIles22 <ilesanderson01@gmail.com>
2026-08-21 22:42:25 -0500

    chore: split jest config into unit/integration projects with separate coverage rules

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,33 @@
 module.exports = {
-  testEnvironment: 'node',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    '<rootDir>/src/controllers/*.js',
-    '<rootDir>/src/middleware/*.js',
-    '<rootDir>/src/services/*.js',
-    '<rootDir>/src/repositories/*.js',
-    '!src/server.js',
-    '!tests/helpers/*'
-    
-  ],
-  testMatch: ['**/tests/**/*.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'] 
+  projects:[
+    {
+      displayName:'integration',
+      testEnvironment: 'node',
+      collectCoverageFrom: [
+        '<rootDir>/src/controllers/*.js',
+        '<rootDir>/src/middleware/*.js',
+        '<rootDir>/src/services/*.js',
+        '<rootDir>/src/repositories/*.js',
+        '!src/server.js',
+        '!tests/helpers/*'
+      ],
+      testMatch: ['**/tests/integration/*.test.js'],
+      setupFilesAfterEnv: ['<rootDir>/tests/setup.js'] 
+    },
+    {
+      displayName:'unit',
+      testEnvironment: 'node',
+      collectCoverageFrom: [
+        'src/services/*'
+      ],
+      coveragePathIgnorePatterns:[
+        '<rootDir>/src/config/*',
+        '<rootDir>/src/constants/*',
+        '<rootDir>/src/repositories/*',
+        '<rootDir>/src/utils/*'        
+      ],
+      testMatch: ['**/tests/unit/*.test.js']
+    }
+  ]
 };
